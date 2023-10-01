500cdf68fcf8ff52f4888e1448fa2702a776e2e9
hama247 <122891188+hama247@users.noreply.github.com>
2023-10-01 17:38:56 +0900

    Update password_manager.sh

--- password_manager.sh
+++ password_manager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# パスワード情報を保存するファイルのパス
 PASSWORD_FILE="passwords.txt"
+ENCRYPTED_FILE="passwords.txt.gpg"
 
 echo "パスワードマネージャーへようこそ！"
 
@@ -17,17 +17,21 @@ while true; do
             read username
             echo "パスワードを入力してください："
             read password
-
-            # 入力された情報をファイルに保存
+            
+            # 復号化して追記
+            gpg --decrypt $ENCRYPTED_FILE 2> /dev/null > $PASSWORD_FILE || true
             echo "$service:$username:$password" >> $PASSWORD_FILE
-
-            echo "パスワードの追加は成功しました。"
+            # 再度暗号化
+            gpg -c $PASSWORD_FILE
+            rm $PASSWORD_FILE
             ;;
 
         "Get Password")
             echo "サービス名を入力してください："
             read service
-            # 入力されたサービス名に対応する情報を抽出
+            
+            # 一時的に復号化して検索
+            gpg --decrypt $ENCRYPTED_FILE 2> /dev/null > $PASSWORD_FILE || true
             result=$(grep "^$service:" $PASSWORD_FILE)
 
             if [ -z "$result" ]; then
@@ -38,13 +42,14 @@ while true; do
                 echo "ユーザー名：${parts[1]}"
                 echo "パスワード：${parts[2]}"
             fi
+            
+            rm $PASSWORD_FILE
             ;;
 
         "Exit")
             echo "Thank you!"
-            break
+            exit 0
             ;;
-
         *)
             echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
             ;;
